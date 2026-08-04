dc753ad5ad05f13fa7c97097d43e9d2b546df0ef
sahilthakur13 <thakur13092003@gmail.com>
2026-08-04 13:04:50 +0530

    push correct to main

--- server/services/imagekitStorageService.js
+++ server/services/imagekitStorageService.js
@@ -6,7 +6,6 @@ const client = new ImageKit({
 })
 
 async function fileUploads(file){
-    console.log("file after decoding from raw buffer to string ....",file)
     const result = await client.files.upload({
         file,
         fileName:"bookBuddyBannerImage" + Date.now(),
